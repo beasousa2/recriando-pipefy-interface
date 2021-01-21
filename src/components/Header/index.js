220114e0e6c04fe0e1a62fa4fbc6e62d1f174a65
Beatriz Sousa <beasousa20@outlook.com>
2021-01-21 16:30:41 -0300

    Inserindo projeto no repositorio

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Header() {
+  return <Container>
+    <h1>Reactify</h1>
+  </Container>;
+}
+
+export default Header;
